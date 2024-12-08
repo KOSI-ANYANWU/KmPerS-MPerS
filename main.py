5f3e243075bb2ab891fee5103b85b53fe58fb38b
Kosi Anyanwu Jaden <Infinitrix900@gmail.com>
2024-12-08 14:25:07 +0100

    FUNCTTION for mathematical challenges

---
+++ main.py
@@ -0,0 +1,34 @@
+def greet(name):
+    print("hello",name)
+
+greet("KOSI")
+# write a function to add 2 numbers,those two numbers should come from the parameter of that function
+def add(number1,number2):
+    print(number1+number2)
+
+add(9,9)
+
+# write a function to subtract 2 numbers,those two numbers should come from the parameter of that function
+def subtract(number1,number2):
+    print(number1-number2)
+
+subtract(9,1)
+# write a function to multiply 2 numbers,those two numbers should come from the parameter of that function
+def multiply(number1,number2):
+    print(number1*number2)
+
+multiply(5,8)
+
+# write a function to do a sqaure of a given number it should come from parameter of that function
+def square(number1):
+    print(number1*number1)
+square(2)
+# write a function to do a division of a given number it should come from parameter of that function
+def divide(number1,number2):
+    print(number1/number2)
+
+divide(10,2)
+
+# write a function to caculate the power of a given number, number and power should come from parameter
+def power(number1):
+    print(number1)
